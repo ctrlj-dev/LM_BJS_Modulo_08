2218057f4f10c646ee5bdf1a238b1dd657653a57
Jesu1701 <jesuscc1701@gmail.com>
2020-10-27 21:41:13 +0100

    cambios menores

--- src/dist/src.e31bb0bc.js
+++ src/dist/src.e31bb0bc.js
@@ -442,22 +442,6 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-//import * as clientBusiness from "./js/client-business.js";
-//import * as styleBusiness from "./js/style-business.js";
-//import * as accountBusiness from "./js/account-business.js";
-
-/*
-clientBusiness.getClientElement();
-clientBusiness.getClientNode();
-clientBusiness.getFullName();
-
-clientListBusiness.printClientsAccounts();
-
-styleBusiness.getNodeStyle();
-
-accountBusiness.getClientAccountsElement();
-accountBusiness.getClientAccountsElement();
-*/
 window.onload = function () {
   clientListBusiness.printClientsAccounts();
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-//import * as clientBusiness from "./js/client-business.js";
 import * as clientListBusiness from "./js/client-list-business.js";
-//import * as styleBusiness from "./js/style-business.js";
-//import * as accountBusiness from "./js/account-business.js";
-
-/*
-clientBusiness.getClientElement();
-clientBusiness.getClientNode();
-clientBusiness.getFullName();
-
-clientListBusiness.printClientsAccounts();
-
-styleBusiness.getNodeStyle();
-
-accountBusiness.getClientAccountsElement();
-accountBusiness.getClientAccountsElement();
-*/
 
 window.onload = function() {
     clientListBusiness.printClientsAccounts();
